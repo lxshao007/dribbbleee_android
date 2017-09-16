c34463b98418fe451e67f12e7702412ea6fa6ece
lingxiao <lingxiaoahao@gmail.com>
2017-09-16 19:48:31 -0400

    implemented shot list view

---
+++ app/src/main/java/com/example/lingxiao/dribbbleee/View/ShotList/ShotListFragment.java
@@ -0,0 +1,30 @@
+package com.example.lingxiao.dribbbleee.View.ShotList;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by lingxiao on 9/15/17.
+ */
+
+public class ShotListFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
